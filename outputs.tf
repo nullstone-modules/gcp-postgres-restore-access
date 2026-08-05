3738bab444ab2ff50c1741e493e486c16613b274
Brad Sickles <brad.sickles@gmail.com>
2026-08-04 21:32:23 -0400

    Added missing env vars and secrets

--- outputs.tf
+++ outputs.tf
@@ -4,14 +4,30 @@ output "env" {
       name  = "RESTORE_OWNER_ROLE"
       value = local.owner_role
     },
+    {
+      name  = "POSTGRES_HOST"
+      value = local.db_subdomain
+    },
+    {
+      name  = "POSTGRES_USER"
+      value = local.username
+    },
+    {
+      name  = "POSTGRES_DB"
+      value = local.admin_database
+    }
   ]
 }
 
 output "secrets" {
   value = [
     {
+      name  = "POSTGRES_PASSWORD"
+      value = random_password.this.result
+    },
+    {
       name  = "POSTGRES_URL"
       value = local.postgres_url
-    },
+    }
   ]
 }
--- postgres.tf
+++ postgres.tf
@@ -5,6 +5,8 @@ data "ns_connection" "postgres" {
 
 locals {
   db_endpoint       = data.ns_connection.postgres.outputs.db_endpoint
+  db_subdomain      = split(":", local.db_endpoint)[0]
+  db_port           = split(":", local.db_endpoint)[1]
   postgres_ssl_mode = try(data.ns_connection.postgres.outputs.postgres_ssl_mode, "prefer")
 
   db_admin_func_url = data.ns_connection.postgres.outputs.db_admin_function_url
